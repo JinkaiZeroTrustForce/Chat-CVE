91e352e83b500b327b1ea148bc23b387a4a327f6
kajikaji0725 <kajiharalivi@icloud.com>
2025-09-08 13:49:05 +0900

    add: Add the base program for the backend

---
+++ backend/create_question.py
@@ -0,0 +1,46 @@
+import os
+
+import dotenv
+from openai import OpenAI
+
+
+def chat_with_gpt(system_prompt: str, prompt: str) -> str:
+    dotenv.load_dotenv()
+
+    client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=1,
+        max_tokens=4096,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    text = (
+        response.choices[0].message.content.strip()
+        if response.choices[0].message.content is not None
+        else ""
+    )
+    return text
+
+
+def create_questions(langage: str):
+    #　プロンプトが確定次第追加
+    user_prompt = ""
+    system_prompt = ""
+    questions = chat_with_gpt(system_prompt, user_prompt)
+    return questions
+
+
+def evaluate_result(evaluation_request):
+    #　プロンプトが確定次第追加
+    user_prompt = ""
+    system_prompt = ""
+    eval = chat_with_gpt(system_prompt, user_prompt)
+
+    return eval
---
+++ backend/main.py
@@ -0,0 +1,88 @@
+from typing import List
+
+import create_question
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, TypeAdapter
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # local
+    ],
+    # allow_origins = [
+    #     "*"
+    # ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# 生成した問題をFrontendに送信する型
+class GenerateQuestionResponse(BaseModel):
+    level: int
+    language: str
+    source_code: str
+    answer: str
+    explanation: str
+
+
+# ユーザが回答した内容をFrontendから受信する型
+class EvaluateQuestionRequest(BaseModel):
+    level: int
+    language: str
+    source_code: str
+    answer: str
+    explanation: str
+    user_answer: str
+    user_explanation: str
+
+
+# 総合評価の各問題の採点詳細
+class EvaluateQuestionDetail(BaseModel):
+    index: int
+    level: str
+    position_score: float
+    reason_score: float
+    weighted_score: float
+    feedback: str
+
+
+# 総合評価をFrontendに送信する型
+class EvaluateQuestionResponse(BaseModel):
+    total_score: int
+    grade: str
+    per_item: List[EvaluateQuestionDetail]
+    summary: str
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+
+@app.get("/generate_question", response_model=List[GenerateQuestionResponse])
+def generate_scenario(langage: str):
+    questions_response = create_question.create_questions(langage=langage)
+    questions_response = TypeAdapter(List[GenerateQuestionResponse]).validate_json(
+        questions_response
+    )
+    return questions_response
+
+
+@app.post("/evaluation", response_model=EvaluateQuestionResponse)
+def evaluation_result(evaluation_requests: List[EvaluateQuestionRequest]):
+    evaluation_response = create_question.evaluate_result(
+        [
+            evaluation_request.model_dump_json()
+            for evaluation_request in evaluation_requests
+        ]
+    )
+    evaluation_response = TypeAdapter(EvaluateQuestionResponse).validate_json(
+        evaluation_response
+    )
+
+    return evaluation_response
