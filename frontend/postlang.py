4e49df191505ace7ea841e37ebda62c43bf579ae
RT859 <hnmkdsxfcgvhbjn@gmail.com>
2025-09-29 18:04:12 +0900

    9/29

--- frontend/postlang.py
+++ frontend/postlang.py
@@ -36,34 +36,34 @@ def receive():
     question = data.get("source code",data.get("source code", ""))
     answer = data.get("answer")
     explanation = data.get("explanation")
-    #------------------------------------回答と説明をansewerに送信------------------------------------------------------------
-    url="http://localhost:5000/answer"
     
-    params = {"answer": answer,"explanation": explanation}
-    response = requests.post(url, params=params)#answerに送信
-
-    return render_template("/input.html",level=level,question=question)
+    input(level=level,question=question,answer=answer,explanation=explanation)
+    return render_template("/input.html",level=level,question=question) #input.htmlにレベル、問題、模範解答、模範解説を送る
 
 @app.route("/input", methods=["GET","POST"])
-def input():#入力された情報をバックに送る、
+def input():#入力された情報をバックとlogに送る、
+    explanation=explanation
+    answer=answer
     level = request.form.get("level")
     question = request.form.get("question")
     input1 = request.form['where']
     input2 = request.form['why']
+   
     #url="http://localhost:5173/generate_question" #バック送信URL
     urlLog="http://localhost:5000/log"
-    #url="http://localhost:5000/debug"#デバッグ用
+    url="http://localhost:5000/debug"#デバッグ用
 
     if(input1 and input2):
         params = {"where": input1,"why":input2}
-        paramsLog = {"level": level,"question": question,"where": input1,"why":input2}
-        responseLog = requests.post(urlLog, params=paramsLog)#ログに送信,デバッグ用
+        #paramsLog = {"level": level,"question": question,"where": input1,"why":input2}
+        #responseLog = requests.post(urlLog, params=paramsLog)#ログに送信,デバッグ用
         
-       #response = requests.post(url, params=params)#バックにjson
-        return  answer()
+        response = requests.post(url, params=params)#バックにjson
+        answer()
+        return   render_template("/input.html",level=level,question=question,answer=answer,explanation=explanation)
     #return render_template("/answer.html",where=input1, why=input2)#デバッグ用だからいらん
 
-    return render_template("/input.html",level=level,question=question)
+    return render_template("/input.html",level=level,question=question,where=input1, why=input2)
 
 @app.route("/answer", methods=["GET","POST"])#回答、
 def answer():
@@ -81,26 +81,27 @@ def answer():
 #},
 #--------------------------------------------------------------------------------
     data = request.get_json()#バックからスコアなどを受け取る
-   
+
     level = request.form.get("level")
     question = request.form.get("question")
     point = data.get("total_score",data.get("total_score", ""))
     feeback=data.get("per_item",data.get("per_item", "")  ) 
     answer = request.form.get("answer")#受け取れないようだったらrequest.args.get("answer")
     explanation = request.form.get("explanation")#受け取れないようだったらrequest.args.get
-    
+    #バックからのjsonでanswerとexplanation受け取れたほうが嬉しいかも
 
     if(level==5):
         return render_template("/log.html")#五問解き終わったらlog.htmlに問題、レベル、answer、explanation、point、feedbackとか適当に送る
     
-    return render_template("/input.html",level=level,question=question,total_score=point,feeback=feeback,answer=answer,explanation=explanation)#inputに反映
+    return render_template("/answer.html",level=level,question=question,total_score=point,feeback=feeback,answer=answer,explanation=explanation)#inputに反映
+    #return render_template("/input.html",level=level,question=question,total_score="a",feeback="b",answer=answer,explanation=explanation)#inputに反映
 
 
 @app.route("/log", methods=["GET","POST"])#回答、
 def log():
 
 
-    return ""
+    return sendLang()
 
 @app.route("/debug", methods=["GET","POST"])#デバッグ用、書かない
 def debug():
